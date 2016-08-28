f0c7b56578aca44ccf1d072d5aedc6310d208c4d
HelloWorllds <dark____angel@mail.ru>
2016-08-28 07:18:43 +0300

    In Front-End add some animation: In Back-End add db file and install mongo with mongoose

--- public/js/common.js
+++ public/js/common.js
@@ -3,20 +3,42 @@ $(document).ready(function() {
 	function wResize() {
 		var height = $(window).height() - 20;
 		$("body").css("min-height", height);
-	};
+	}
+
 	wResize();
+
 	$(window).resize(function() {
 		wResize();
 	});
 
-	
+	// $("#nav-toggle").click(function() {
+	// 	$(".form-container").css("display", "block");
+	// });
+
+	$(".form-container form button").click(function() {
+		$(".form-container").css("display", "none");
+	});
+
+	$("#nav-toggle").click(function() {
+		var menuPosition = $(".main-menu-container").css("left");
+		if (menuPosition === "-205px") {
+			$(".main-menu-container").animate({
+				left: "-10px"
+			});
+		}
+		if (menuPosition === "-10px") {
+			$(".main-menu-container").animate({
+				left: "-205px"
+			});
+		}
+	});
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
-	};	
+	}
 });
 
 document.querySelector("#nav-toggle").addEventListener("click", function() {
